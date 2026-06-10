700ce88d8af5b6601b8a9cf6e8ab091a4b98a67d
Sven Beck <s.beck@student.maastrichtuniversity.nl>
2026-06-10 17:17:14 +0200

    changes of home and away scores to numeric in data cleaning

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 matches <- read_csv("data-raw/results.csv")
 
-worldcup_matches <- matches %>% filter(tournament == "FIFA World Cup")
+worldcup_matches <- matches %>% filter(tournament == "FIFA World Cup") %>% mutate(home_score = as.numeric(home_score),
+                                                                                  (away_score = as.numeric(away_score))
+
+                                                                                  )
 
 usethis::use_data(worldcup_matches, overwrite = TRUE)
