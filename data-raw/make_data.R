637b516bd4a139bdb8924b42adad781d8d240db0
Sven Beck <s.beck@student.maastrichtuniversity.nl>
2026-06-10 18:12:56 +0200

    update document()

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -4,6 +4,9 @@ matches <- read_csv("data-raw/results.csv")
 worldcup_matches <- matches %>% filter(tournament == "FIFA World Cup") %>% mutate(home_score = as.numeric(home_score),
                                                                                   (away_score = as.numeric(away_score))
 
-                                                                                  )
+                                                                                  ) %>% filter(!is.na(home_score),
+                                                                                               !is.na(away_score)
+  )
 
 usethis::use_data(worldcup_matches, overwrite = TRUE)
+
