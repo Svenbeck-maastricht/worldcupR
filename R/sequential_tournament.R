b49d326b5e272ad6a53bec2e52b4b74f05749947
Sven Beck <s.beck@student.maastrichtuniversity.nl>
2026-06-11 16:52:44 +0200

    naming mismatch between output of previous function fixed to run the loop

--- R/sequential_tournament.R
+++ R/sequential_tournament.R
@@ -1,7 +1,8 @@
 tournament_seq <- function(teams){
   winner <- character(500)
   for (i in 1:500){
-    winner[i] <- simulate_tournament(teams)
+    winner[i] <- simulate_tournament(teams)$champion
   }
   return(winner)
 }
+
