fb85dd8a0c94662f76453ccfeadf9fda0dd9de73
Sven Beck <s.beck@student.maastrichtuniversity.nl>
2026-06-11 18:33:04 +0200

    added package dependency and exports

--- R/parallel_tournament.R
+++ R/parallel_tournament.R
@@ -4,7 +4,7 @@ library(doParallel)
 
 tournament_par <- function(teams){
   winner <- character(500)
-  foreach (i = 1:500, .combine = c) %dopar% {
+  foreach (i = 1:500, .combine = c, .export = c("simulate_tournament", "simulate_match", "team_summary", "worldcup_matches"), .packages = c("dplyr")) %dopar% {
     simulate_tournament(teams)$champion
   }
 }
