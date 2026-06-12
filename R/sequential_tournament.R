1d72405e57b80c6c03f09945b8a393feb7051678
Sven Beck <s.beck@student.maastrichtuniversity.nl>
2026-06-12 14:39:29 +0200

    roxygen2 updated

--- R/sequential_tournament.R
+++ R/sequential_tournament.R
@@ -1,3 +1,13 @@
+#' Sequential Tournament Simulation
+#'
+#' @param teams  character vector of 8 teams
+#'
+#' @return character vector of winners
+#' @export
+#'
+#' @examples
+#' top8_teams <- c("Brazil", "Germany", "France", "Netherlands", "Argentina", "Spain", "England", "Portugal")
+#' tournament_seq(top8_teams)
 tournament_seq <- function(teams){
   winner <- character(500)
   for (i in 1:500){
